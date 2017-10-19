7357480c4a9e2a3e33b9d0369b465fa8ad0b87c0
Nell Park <napark8@gmail.com>
2017-10-19 10:26:39 -0400

    Update DayofDate.java

--- DayofDate.java
+++ DayofDate.java
@@ -1,4 +1,6 @@
 /* http://www.programmr.com/challenges/find-day-given-date-1
+ * 2017/09/22 Friday
+ *
  * Question:
  *
  * Write a program to takes input date as [dd/mm/yyyy] formate and find out the day of entered date.
@@ -18,82 +20,72 @@ import java.io.*;
 import java.util.Scanner;
 import java.lang.Math;
 
-
-
-
-
 public class DayofDate
 {
-    public static boolean isLeap(int year){
-  /*a year is leap year (with 366 days) if evenly divisible by 4
-    or by 100 AND 400 at the same time */
-  if(year%4 == 0) return true;
-  else return false;
-}
+	public static boolean isLeap(int year){
+		/*a year is leap year (with 366 days) if evenly divisible by 4
+		or by 100 AND 400 at the same time */
+		if(year%4 == 0) return true;
+		else return false;
+	}
 
-public static int absoluteDays(int year, int month, int date){
 	/*find how many days passed since 0000/01/01 */
-	int daysPassed = 0;
-	for(int i = 0; i<year; i++){
-		if(isLeap(i)) daysPassed +=366;
-		else daysPassed+=365;
-	}
-	//calculating days in months
-	if(isLeap(year)){ 
-		int[] daysPerMonthL = {31,29,31,30,31,30,31,31,30,31,30,31};
-		for(int i = 0; i<month-1;i++){
-			daysPassed+=daysPerMonthL[i];
+	public static int absoluteDays(int year, int month, int date){
+		int daysPassed = 0;
+		for(int i = 0; i<year; i++){
+			if(isLeap(i)) daysPassed +=366;
+			else daysPassed+=365;
 		}
-	}
-	else{ 
-		int[] daysPerMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
-		for(int i = 0; i<month-1;i++){
-			daysPassed+=daysPerMonth[i];
+		//calculating days in months
+		if(isLeap(year)){ 
+			int[] daysPerMonthL = {31,29,31,30,31,30,31,31,30,31,30,31};
+			for(int i = 0; i<month-1;i++){
+				daysPassed+=daysPerMonthL[i];
+			}
 		}
+		else{ 
+			int[] daysPerMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+			for(int i = 0; i<month-1;i++){
+				daysPassed+=daysPerMonth[i];
+			}
+		}
+		//calculate days
+		daysPassed+=date;
+		return daysPassed;
 	}
-	//calculate days
-	daysPassed+=date;
-	return daysPassed;
-}
-	/**
-	 * @param args
-	 */
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-		       
-	        //write your logic here
-			Scanner sc = new Scanner(System.in);
-			//input format is dd/mm/yyyy
-			String dateS = sc.next();
-			int year = Integer.valueOf(dateS.substring(6));
-			int date = Integer.valueOf(dateS.substring(0,2));
-			int month = Integer.valueOf(dateS.substring(3,5));
 
-			/*  pick a reference day given in the example
-				12/12/1988 is Monday
-			*/
-			//int year = 1985; int date = 1; int month = 1;
-			int max = Math.max(year, 1988);
-			int min = Math.min(year,1988);
+		//write your logic here
+		Scanner sc = new Scanner(System.in);
+		//input format is dd/mm/yyyy
+		String dateS = sc.next();
+		int year = Integer.valueOf(dateS.substring(6));
+		int date = Integer.valueOf(dateS.substring(0,2));
+		int month = Integer.valueOf(dateS.substring(3,5));
 
-			int daysPassed = Math.abs(absoluteDays(year,month,date) - absoluteDays(1988,12,12));
-			int dayConversion = daysPassed%7;
-			int indicator = 0;
-			if (max == 1988) {
-				if (month <= 12 && date <= 12) indicator = 1;
-			}
+		/*  pick a reference day given in the example
+			    12/12/1988 Monday
+		*/
+		int max = Math.max(year, 1988);
+		int min = Math.min(year,1988);
 
-			//days cover two weeks to trace both ways (backward and forward)
-			String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Monday"};
-			String result = "";
-			if(indicator == 1){
-				//need to trace days backward from Monday
-				System.out.println("Day is " + days[7-dayConversion]); 
-			}
-			else System.out.println("Day is " + days[dayConversion]);
-                //end 
-        }
+		int daysPassed = Math.abs(absoluteDays(year,month,date) - absoluteDays(1988,12,12));
+		//offset from reference day: Monday
+		int dayConversion = daysPassed%7;
+		int indicator = 0;
+		if (max == 1988) {
+			if (month <= 12 && date <= 12) indicator = 1;
+		}
+
+		//list of days to trace both ways (backward and forward)
+		String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Monday"};
+		String result = "";
+		if(indicator == 1){
+			//need to trace days backward from Monday
+			System.out.println("Day is " + days[7-dayConversion]); 
+		}
+		else System.out.println("Day is " + days[dayConversion]);
+		//end 
+	}
 }
